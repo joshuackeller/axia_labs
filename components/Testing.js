74ccc74383a591e90a9c4bb5c65c019b7dce3ec9
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2021-10-25 04:21:53 -0600

    remove test file

--- components/Testing.js
+++
@@ -1,8 +0,0 @@
-export default function Testing() {
-    return(
-        <div>
-            <h1 className="text-2xl">This is the header</h1>
-            <p>This is a test</p>
-        </div>
-    )
-}
