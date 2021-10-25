c8122fd517718a2200fe9d6502cd9be0697ed34d
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2021-10-25 04:18:31 -0600

    day 3

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,75 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import styled from 'styled-components'
+import { ThreeBars } from '@styled-icons/octicons'
+import { X } from '@styled-icons/boxicons-regular'
+
 
 export default function Navbar() {
+
+    const [isOpen, setOpen] = useState(false)
+
+
     return(
-        <div className="mx-24 my-8 flex justify-between bg-transparent">
-            <div>
-            <h1 className="font-black text-logo">Axia Labs</h1>
+        <div>
+            <div className="flex justify-center items-center ">
+                <div className="w-11/12 my-8 flex justify-between bg-transparent">
+                    <div>
+                    <Link href="/">
+                        <a>
+                            <h1 className="font-black text-logo">Axia Labs</h1>
+                        </a>
+                    </Link>
+                    </div>
+                    <div className="space-x-8 hidden md:block">
+                        <Link href="/">
+                            <a className="text-gray-400 hover:text-axiablack transition duration-300 font-light 
+                            tracking-wide text-sm">
+                                Portfolio
+                            </a>
+                        </Link>
+                        <Link href="/">
+                            <a className="bg-axiapink font-bold text-sm px-8 py-3 rounded-lg shadow-sm hover:bg-fadedpink
+                                transition duration-300">
+                                Contact Us
+                            </a>
+                        </Link>
+                    </div>
+                    <div className="md:hidden">
+                        <div className="md:hidden"
+                            style={{
+                                display: isOpen ? 'none' : 'block'
+                            }}>
+                            <ThreeBars className="h-5"
+                            styled={{color: 'black'}}
+                            onClick={() => setOpen(true)}/>
+                        </div>
+                        <div className="md:hidden"
+                            style={{
+                                display: isOpen ? 'block' : 'none'
+                            }}>
+                            <X className="h-7"
+                            onClick={() => setOpen(false)}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="space-x-8">
-                <Link href="/">
-                    <a className="text-gray-400 hover:text-axiablack transition duration-300 font-light 
-                    tracking-wide">
+            <div className="md:hidden">
+                <div className="transform transition scale-y-100 duration-500"
+                style={{
+                    display: isOpen ? 'block' : 'none'
+                }}>
+                    <div className="text-gray-400 hover:text-axiablack text-center transition duration-300 font-light 
+                                tracking-wide py-3 text-sm">
                         Portfolio
-                    </a>
-                </Link>
-                <Link href="/">
-                    <a className="bg-axiapink font-black text-sm px-6 py-3 rounded-lg shadow-sm hover:bg-fadedpink
-                        transition duration-300">
+                    </div>
+                    <div className="bg-axiapink font-bold text-sm px-8 text-center shadow-sm hover:bg-fadedpink
+                                    transition py-3 duration-300 ">
                         Contact Us
-                    </a>
-                </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
+
